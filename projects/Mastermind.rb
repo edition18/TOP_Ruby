37664a374d4e54d5223eec4144bb35cf669f90fa
ed <jchoo004@e.ntu.edu.sg>
2020-12-04 05:52:39 +0000

    added Mastermind

---
+++ projects/Mastermind.rb
@@ -0,0 +1,105 @@
+class GuessOutcome
+  attr_reader :guess,:black,:white,:incorrect
+  def initialize(guess)
+    @guess = guess
+    @outcome = Array.new(4)
+  end
+end
+
+
+class Game
+  attr_accessor :guess_count
+  def initialize  
+    @colors = ["Blue","Green","Red","Orange"]
+    @answer = generate_answer
+    @guess_count = 0
+    @board = Array.new(12) {Array.new()}
+  end
+
+  def play
+    #loop do
+      print "\n" + "take your turn"
+      # choice = make_turn
+      @guess_count == 12 ? (print "You Lost"; return) : ""
+      evaluate_guess?(human_choice)
+    #end
+  end
+
+
+  def generate_answer
+
+    answer = []
+    
+    loop do
+
+      answer.push(@colors.sample)
+      if answer.length == 4
+        break
+      end
+    end
+    print "answer is #{answer}"
+    return answer
+  end
+
+  # def evaluate_guess?(guess)
+  #     i = 0
+  #     while i < guess.length do
+  #       guess[i] == @answer[i] ? "" : (add_guess_count; return false) 
+  #     i = i + 1
+  #     end
+  #     add_guess_count
+  #     return true 
+  # end
+
+  def evaluate_guess?(guess)
+    
+    add_guess_count
+    newGuess = GuessOutcome.new(guess)
+    answer_sorted = @answer.sort
+    # I think its easier to find the number of white balls first
+    guess.sort.each_with_index do |guess, i|
+      newGuess.@outcome[i] = 1
+      # guess[i] == answer_sorted[i] ? (newGuess.outcome[i] = 0; p "true") : ("")
+    end
+
+    print newGuess
+    
+
+    return true 
+  end
+
+  def add_guess_count
+    @guess_count = @guess_count + 1
+    #print "\n" + "Current Guess Count: #{@guess_count}"
+  end
+
+
+  def human_choice
+    choice_array = []
+    loop do
+      print "\n" + "choose a color"
+
+      @colors.each_with_index do |color, key|
+        print "\n" + "#{key} #{color}"
+      end
+      input = gets.chomp.to_i
+      
+      while !@colors[input] do
+          print "reenter valid values"
+          input = gets.chomp.to_i
+      end
+      choice_array.push(@colors[input])
+      print "i chose "
+      print choice_array
+
+      if choice_array.length == 4
+        return choice_array
+      end
+    end
+  end
+
+
+end
+
+#Game.new.generate_answer
+Game.new.play
